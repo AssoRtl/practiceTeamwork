6960be38e6341eb44e8fa1633168913cc4eea226
UserAlen <93518707+UserAlen@users.noreply.github.com>
2022-12-08 11:21:28 +0300

    all layouts done

---
+++ app/src/main/java/com/example/a1_2/MainSecond.java
@@ -0,0 +1,14 @@
+package com.example.a1_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainSecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+    }
+}
---
+++ app/src/main/java/com/example/practic/Mob_Publication.java
@@ -0,0 +1,13 @@
+package com.example.practic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mob_Publication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mob_publication);
+    }
+}
