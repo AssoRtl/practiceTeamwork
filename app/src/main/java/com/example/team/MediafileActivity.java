86418cb91d641034c7735fcacce0245e42531d51
UserAlen <93518707+UserAlen@users.noreply.github.com>
2022-12-15 16:18:27 +0300

    Revert "Revert "Переходы с activity_mediafile.xml в activity_feedback.xml, activity_publication.xml, activity_reward.xml;""
    This reverts commit 03b33b08db3891b25018125794fca56cc5c97811.

--- app/src/main/java/com/example/team/MediafileActivity.java
+++ app/src/main/java/com/example/team/MediafileActivity.java
@@ -2,7 +2,9 @@ package com.example.team;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.practic.R;
 
@@ -13,4 +15,21 @@ public class MediafileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mediafile);
     }
+
+    public void reward(View view) {
+        Intent intent = new Intent(getApplicationContext(), RewardActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void publication(View view) {
+        Intent intent = new Intent(getApplicationContext(), PublicationActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void feedback(View view) {
+        Intent intent = new Intent(getApplicationContext(), FeedbackActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
