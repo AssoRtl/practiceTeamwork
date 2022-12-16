1a29c50cb3b6f6c0e98eba6ca1f6ee9383ca6378
UserAlen <93518707+UserAlen@users.noreply.github.com>
2022-12-16 11:47:28 +0300

    feedback firebase

--- app/src/main/java/com/example/team/FeedbackActivity.java
+++ app/src/main/java/com/example/team/FeedbackActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.practic.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class FeedbackActivity extends AppCompatActivity {
+
+    private EditText name, phoneNumber, feedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +27,26 @@ public class FeedbackActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+    public void send(View view) {
+        name = (EditText) findViewById(R.id.personName);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        feedback = (EditText) findViewById(R.id.yourFeedback);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put("name", name.getText().toString());
+        user.put("phoneNumber", phoneNumber.getText().toString());
+        user.put("feedback", feedback.getText().toString());
+        database.collection("users")
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    name.setText("");
+                    phoneNumber.setText("");
+                    feedback.setText("");
+                    showToast("Данные отправлены");
+                }).addOnFailureListener(exception -> showToast(exception.getMessage()));
+    }
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
 }
